c082694e0626309af659c366a64e8ae26b55e1bb
WataruOjima <wataru.ojima20@gmail.com>
2021-01-23 22:47:02 +0900

    商品名で検索

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,21 @@
 class ProductsController < ApplicationController
   before_action :search_product, only: [:index, :search]
   def index
-    @products = Product.all # 全商品の情報を取得
+    @products = Product.all   # 全商品の情報を取得
+    set_product_columm  # privateメソッド内で定義
   end
 
   def search
-    # binding.pry
     @results = @p.result.includes(:category)  # 検索条件にマッチした商品の情報を取得
   end
 
   private
   def search_product
-  @p = Product.ransack(params[:p]) # 検索オブジェクトを生成
+  @p = Product.ransack(params[:p])  # 検索オブジェクトを生成
   end 
+
+  def set_product_columm 
+  @product_name = Product.select("name").distinct  # 重複なくnameカラムのデータを取り出す
+  end
 
 end
